6ae935f87396e4bae5d7c10fe0b4660f8c486e79
IOsonoTAN <iosonotan@gmail.com>
2024-10-22 19:12:22 +0700

    feat: implement user routes

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    /**
+     * API
+     * - /api/users/1 -> params.id = "1"
+     * - /api/users/1?field=name,surname,email -> params.id = "1", request.nextUrl.searchParams.get("field") = "name,surname,email"
+     * - /api/users?id=1 -> request.nextUrl.searchParams.get("id") = "1"
+     */
+    const id = params.id;
+    if (!id) {
+      return NextResponse.json(
+        {
+          error: "Id is required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const prisma = new PrismaClient();
+    const user = await prisma.user.findFirst({
+      where: {
+        id: +id,
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    return NextResponse.json({ data: user });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to fetch users" },
+      { status: 500 }
+    );
+  }
+}
